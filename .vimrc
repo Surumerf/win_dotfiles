232b1a07db93a168c318110806f36b7e63ee5a68
Surumerf <surumerf@gmail.com>
2018-01-30 22:28:50 +0900

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,32 @@
+set number
+set title
+set ambiwidth=double
+set tabstop=4
+set expandtab
+set shiftwidth=4
+set smartindent
+set nrformats-=octal
+set hidden
+set history=50
+set virtualedit=block
+set whichwrap=b,s,[,],<,>
+set backspace=indent,eol,start
+set wildmenu
+if has("autocmd")
+    filetype plugin indent on
+endif
+syntax on
+colorscheme darkblue
+
+set statusline=%F
+set statusline+=%m
+set statusline+=%r
+set statusline+=%h
+set statusline+=%w
+set statusline+=%=
+set statusline+=[ENC=%{&fileencoding}]
+set statusline+=[LOW=%l/%L]
+set laststatus=2
+set encoding=utf-8
+set fileencoding=utf-8
+
---
+++ PowerShell_profile.ps1
@@ -0,0 +1,31 @@
+set-alias sbtext sublime_text
+set-alias ipy ipython
+set-alias ppt powerpnt
+set-alias word winword
+set-alias em emeditor
+set-alias ex explorer
+set-alias st start
+
+function which {
+    gcm $args | fl
+}
+
+function git_branch {
+	git branch 2>$null |
+	where { -not [System.String]::IsNullOrEmpty($_.Split()[0]) } |
+	% { $bn = $_.Split()[1]
+	Write-Output " ($bn)" }
+}
+
+function prompt {
+	(Get-Host).UI.RawUI.WindowTitle = "Windows PowerShell " + $pwd
+	
+	$gb = git_branch
+	
+	Write-Host $env:USERNAME -NoNewline -ForegroundColor Green
+	Write-Host ":" -NoNewline -ForegroundColor White
+	Write-Host "$pwd" -NoNewline -ForegroundColor Cyan
+	Write-Host "$gb"  -ForegroundColor Red
+	Write-Host "$" -NoNewline -ForegroundColor Yellow
+	return " "
+}
